1ddf0c3daf0f9465e573f2d1c3d6fb464c812314
Juliet <agutu@gmail.com>
2022-04-29 19:44:56 +0300

    add views for our notes model under views

--- base/api/views.py
+++ base/api/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import NoteSerializer
+from base.models import Note
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -31,3 +34,13 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):  
+    user = request.user
+    notes = user.note_set.all()
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)  
